ceda1882a40d5ac0f385753c9f19706017fd7ef9
shineXo <shinexo1@mail.ru>
2023-07-24 22:20:46 +0300

    admin classes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -115,8 +115,8 @@ class Recipe(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Рецепт',
-        verbose_name_plural = 'Рецепты',
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
         ordering = ('-created',)
 
     def __str__(self):
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Subscribe
 
+EMPTY_VALUE = '-пусто-'
 
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'username', 'first_name', 'last_name', 'email'
+        'id', 'username', 'first_name', 'last_name', 'email', 'password'
     )
     search_fields = ('username', 'first_name', 'last_name')
-    list_filter = ('first_name', 'email')
-    empty_value_display = '-empty-'
+    list_filter = ('username', 'email')
+    empty_value_display = EMPTY_VALUE
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    empty_value_display = EMPTY_VALUE
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Subscribe, SubscriptionAdmin)
